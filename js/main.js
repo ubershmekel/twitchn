b6fe7f4649a205ecc45c33819988bb82a4ce193e
Yuval Greenfield <ubershmekel@gmail.com>
2016-04-22 09:42:47 -0700

    Bad commit trying to fix duplicate channels again After rewriting this logic for the 3rd time I found out that moving an IFRAME in the DOM makes it reload which entirely defeats the purpose of avoiding a reload. I'm going to change the logic for positioning to be based on x,y absolute positioning instead of DOM order. This commit is just to remember this code which will soon be erased.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var mydebug = {};
+
 $(function() {
     var defaultStreamsToShow;
     var gameToShow;
@@ -5,7 +7,7 @@ $(function() {
     var streamsContainerId = "streamsContainer";
     var streamsContainer = $('#' + streamsContainerId);
     var instructionsContainer = $('#instructionsContainer');
-    var currentlyShowing = [];
+    var previouslyShowingChannels = [];
     var isAjaxing = false;
 
     function urlGetParams() {
@@ -71,27 +73,74 @@ $(function() {
         player.addEventListener("offline", streamWentOffline);
     }
     
-    function showChannels(channels) {
-        if(channels.equals(currentlyShowing)) {
-            console.log("Same channels as currently showing, not updating");
-            return;
-        }
-
-        for(var i = 0; i < channels.length; i++) {
-            var channelName = channels[i];
-            if(currentlyShowing.length > i) {
+    function moveIndexToIndex(jq, indexSrc, indexDst) {
+        var src = jq.children()[indexSrc];
+        var dst = jq.children()[indexDst];
+        jq[0].insertBefore(src, dst);
+    }
+    
+    //streamsContainer[0].insertBefore(src, dst);function moveElement(container, be)
+    
+    function showChannels(newChannels) {
+        //if(channels.equals(previouslyShowing)) {
+        //    console.log("Same channels as currently showing, not updating");
+        //    return;
+        //}
+        var previousElements = streamsContainer.children();
+        for(var i = 0; i < newChannels.length; i++) {
+            var channelName = newChannels[i];
+            var previouslyShowingChannelsIndex = previouslyShowingChannels.indexOf(channelName);
+            if(previouslyShowingChannelsIndex > -1) {
+                //if(currentlyShowingIndex == i) {
+                //    // stay where you are
+                //    console.log("Correctly placed");
+                //    continue;
+                //}
+                console.log("repositioning " + previouslyShowingChannelsIndex + " to " + i);
+                streamsContainer[0].appendChild(previousElements[previouslyShowingChannelsIndex]);
+                
+                // just move the stram to position
+                //moveIndexToIndex(streamsContainer, currentlyShowingIndex, i);
+                //var previousEl = previousElements[previouslyShowingChannelsIndex];
+                //streamsContainer[0].insertBefore(src, dst);
+                //currentlyShowing.splice(currentlyShowingIndex, 1);
+                //currentlyShowing.splice(currentlyShowingIndex, 0, channelName);
+                continue;
+            }
+            /*
                 if(currentlyShowing[i] == channelName)
                     continue;
                 else {
                     streamsContainer.children()[i].remove();
                     currentlyShowing.splice(i, 1);
                 }
-            }
+            }*/
             
-            console.log(channelName);
-            currentlyShowing.push(channelName);
+            // New stream, create it, put it at the end
+            console.log("Embedding new: " + channelName);
             embedTwitch(channelName);
+            /*
+            if(i < currentlyShowing.length) {
+                // not the last stream
+                // based on assumption that twitch embed is the last element in the streams container
+                // we move that element to the correct index.
+                var el = streamsContainer.children().last();
+                var beforeThisEl = streamsContainer.children()[i];
+                streamsContainer[0].insertBefore(el, beforeThisEl);
+                currentlyShowing.splice(i, 0, channelName);
+            } else {
+                currentlyShowing.push(channelName);
+            }*/
         }
+        
+        // Remove excess that was not appended to end. These elements are at the start.
+        var totalChildren = streamsContainer.children().length;
+        for(var j = newChannels.length; j < totalChildren; j++) {
+            streamsContainer.children()[0].remove();
+        }
+        
+        // Document new state
+        previouslyShowingChannels = newChannels;
     }
 
     function handleData(data) {
@@ -149,6 +198,9 @@ $(function() {
             var threeMinutesInMs = 1000 * 60 * 3;
             setInterval(getTopStreams, threeMinutesInMs);
         }
+
+        mydebug.previouslyShowingChannels = function() { return previouslyShowingChannels;};
+        mydebug.showChannels = showChannels;
     }
     
     
